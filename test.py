106cddde4e65a0183f33a8391f0611b68f6af134
kpollackhinds <kahlil.pollack-hinds@stonybrook.edu>
2023-08-20 20:12:05 -0400

    adjusted audio codec to aac

--- test.py
+++
@@ -1,8 +0,0 @@
-from pytube import YouTube
-YouTube('https://youtu.be/9bZkp7q19f0').streams.get_highest_resolution().download()
-
-yt = YouTube('http://youtube.com/watch?v=9bZkp7q19f0')
-yt.streams
-... .filter(progressive=True, file_extension='mp4')
-... .order_by('resolution')[-1]
-... .download()
